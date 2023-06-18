b8655651cfc4d70332cefa5a5f88209c328e2af4
mrMonos <131591271+mrMonos@users.noreply.github.com>
2023-06-18 15:04:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from colors import *
+commands = dict()
+PROMPT = "(CMD-3)"
+__all__ = "CMD-3.0"
+__name__ = ["CMD-3.0"]
+class CMD():
+    def __init__(self):
+        self.prompt = PROMPT
+        self.intro = """
+        Welcome at CMD-3.0 \n
+        ===========================
+        """
+        print(self.intro)
+        commands.update({"help": self.intro2,})
+    def __init_subclass__(cls):
+        super().__init__(cls)
+    def intro2(self,args):
+        print(args[0]);
+    def prase(self,line,commands):
+        prasedline = line.split()
+        try:
+            self.command = prasedline[0]
+        except:
+            print("You have not given an order to execute")
+        del prasedline[0]
+        self.args = prasedline
+        try:
+            return commands[self.command](self.args)
+        except:
+            return commands[self.command]()
+    def cmdloop(self):
+        line = "init" 
+        while  line != "exit":
+            line = input(self.prompt)
+            if line != "exit":
+                self.prase(line,commands)
+
